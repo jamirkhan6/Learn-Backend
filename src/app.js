ed68ec7b1752eaac39c4123629714c42cd7b53d7
jamirkhan6 <ajzomirkhan6@gmail.com>
2026-02-05 08:52:58 +0600

    send get delete update

--- src/app.js
+++ src/app.js
@@ -1,14 +1,52 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json())
 
+const notes = []
 
-app.get("/", (req, res) => {
-    res.send("jamir khan")
+//send to server
+app.post('/notes', (req, res) => {
+    notes.push(req.body)
+
+    res.status(201).json({ massage : "note created successfully"})
 })
 
-app.get("/about", (req, res) => {
-    res.send("about page")
+// get to server
+app.get('/notes', (req, res) => {
+    
+    res.status(200).json({
+        massage : "notes fetched successfully",
+        notes : notes
+    })
+
+})
+
+// delete in server
+app.delete('/notes/:index', (req, res) => {
+
+    const index = req.params.index
+
+    delete notes[ index ]
+
+    res.status(200).json({
+        message: "note deleted successfully"
+    })
+
+})
+
+// update in server
+app.patch('/notes/:index', (req, res) => {
+
+    const index = req.params.index
+    const description = req.body.description
+
+    notes [ index ].description = description
+
+    res.status(200).json({
+        message: "notes updated successfully"
+    })
+
 })
 
 
